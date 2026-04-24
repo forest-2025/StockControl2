6c3bd5228ef96c2b5dbcbb327cb4b62c17459c0e
forest-2025 <f68022757@gmail.com>
2026-04-24 15:33:40 +0900

    new

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS m_user
 (
-   id int NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+   id int AUTO_INCREMENT PRIMARY KEY,
    email_address VARCHAR (254) NOT NULL UNIQUE,
    password VARCHAR (256) NOT NULL,
    family_name VARCHAR (100) NOT NULL,
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS m_user
 ALTER TABLE m_user ALTER COLUMN ID RESTART WITH 11;
 CREATE TABLE IF NOT EXISTS m_supplier
 (
-   id int NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+   id int AUTO_INCREMENT PRIMARY KEY,
    name VARCHAR (100) NOT NULL,
    furigana VARCHAR (100) NOT NULL,
    is_deleted tinyint NOT NULL default '0',
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS m_supplier
 ALTER TABLE m_supplier ALTER COLUMN ID RESTART WITH 6;
 CREATE TABLE IF NOT EXISTS m_customer
 (
-   id int NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+   id int AUTO_INCREMENT PRIMARY KEY,
    name VARCHAR (100) NOT NULL,
    furigana VARCHAR (100) NOT NULL,
    is_deleted tinyint NOT NULL default '0',
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS m_customer
 ALTER TABLE m_customer ALTER COLUMN ID RESTART WITH 6;
 CREATE TABLE IF NOT EXISTS m_product
 (
-   id int NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+   id int AUTO_INCREMENT PRIMARY KEY,
    name VARCHAR (100) NOT NULL,
    number VARCHAR (50) UNIQUE NOT NULL,
    supplier_id int NOT NULL,
@@ -49,7 +49,7 @@ CREATE INDEX ix_m_product_name ON m_product (name);
 CREATE INDEX ix_m_product_number ON m_product (number);
 CREATE TABLE IF NOT EXISTS t_stock
 (
-   id int NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+   id int AUTO_INCREMENT PRIMARY KEY,
    product_id int NOT NULL,
    stock_quantity int NOT NULL,
    register_date_time datetime NOT NULL default CURRENT_TIMESTAMP,
@@ -59,7 +59,7 @@ CREATE TABLE IF NOT EXISTS t_stock
 ALTER TABLE t_stock ALTER COLUMN ID RESTART WITH 12;
 CREATE TABLE IF NOT EXISTS t_transaction_history
 (
-   id int NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+   id int AUTO_INCREMENT PRIMARY KEY,
    product_id int NOT NULL,
    amount_of_change int NOT NULL,
    supplier_id int,
